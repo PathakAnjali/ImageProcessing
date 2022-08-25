a2d3c9b295ed5e1212f448197265f89b7cbbbbdb
Anjali Pathak <87489165+PathakAnjali@users.noreply.github.com>
2022-08-25 21:36:33 +0530

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,25 +1,23 @@
 import sys
-import os                                     # to grab the file/folder path
+import os                                     
 from PIL import Image
 
-# step1: arguments to hold current dir and  new dir for output  img holder if exists or not
+#arguments to hold current dir and  new dir for output  img holder if exists or not
 
 img_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-# print(img_folder,output_folder)
 
-
-# to check if outputPokedexImg dir available or not
-print(os.path.exists(output_folder))  # gives Boolean value if folder exists or not
+# to check if outputPokedexImg dir available or notprint(os.path.exists(output_folder))  
 
 if not (os.path.exists(output_folder)):
     os.makedirs(output_folder)  # pass new folder name  on terminal as argument which is stored in output_folder
 
-# loop through the files present in the PokedexConvertorImg dir
+# looping through the files
 
-for filename in os.listdir(img_folder):  # as img_folder is the input folder,to loop through
-    img = Image.open(f'{img_folder}/{filename}')  # '/' is added as to navigate the folder and file name
-    clean_name = os.path.splitext(filename)[0]    # to avoid this type filename > charmander.jpg.png > to remove .jpg
+for filename in os.listdir(img_folder):
+    
+    img = Image.open(f'{img_folder}/{filename}')  
+    clean_name = os.path.splitext(filename)[0]   
     img.save(f'{output_folder}/{clean_name}.png', 'png')
     print("done")
